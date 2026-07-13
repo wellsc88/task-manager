0725d5f7c35f00d620837ac270a20b68ae2f5f01
Wellington <133610786+wellsc88@users.noreply.github.com>
2026-07-13 12:44:15 -0300

    feat: create initial task manager domain model (#1)

---
+++ src/main/java/com/well/tech/task/manager/common/enums/TaskPriority.java
@@ -0,0 +1,8 @@
+package com.well.tech.task.manager.common.enums;
+
+public enum TaskPriority {
+
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/com/well/tech/task/manager/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.well.tech.task.manager.repository;
+
+import com.well.tech.task.manager.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {}
